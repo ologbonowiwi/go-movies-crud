f5f9cb7d3965c6f508e3ae3e79f054f4f60a782a
Wesley Matos <wm4tos.777@gmail.com>
2023-02-15 23:54:25 -0300

    docs: add swagger docs

--- go.mod
+++ go.mod
@@ -3,3 +3,23 @@ module github.com/ologbonowiwi/go-movies-crud
 go 1.19
 
 require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/swaggo/http-swagger v1.3.3 // indirect
+	github.com/swaggo/swag v1.8.10 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/ologbonowiwi/go-movies-crud/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 type Movie struct {
@@ -23,17 +25,45 @@ type Director struct {
 
 var movies []Movie
 
+// get all movies
+// @Summary List movies
+// @Description get all movies
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} []Movie
+// @Failure 404 {string} string "No movies found"
+// @Router /movies [get]
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	if len(movies) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("No movies found")
+		return
+	}
+
 	json.NewEncoder(w).Encode(movies)
 }
 
+// @title Movies CRUD API
+// @description This is a sample server for a movie store.
+// @version 1.0.0
+
+// @BasePath /api/v1
 func main() {
 	r := mux.NewRouter()
 
+	// serve swagger docs
+	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
+
 	movies = append(movies, Movie{ID: "1", Isbn: "448743", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "448744", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
-	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	// put all your routes on /api/v1
+	api := r.PathPrefix("/api/v1").Subrouter()
+
+	// add your routes
+	api.HandleFunc("/movies", getMovies).Methods("GET")
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
